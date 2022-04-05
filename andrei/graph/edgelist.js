c23c8054451c7a3c96483f8a8c06b2ef8ce72ab3
David Anusontarangkul <anusontarangkul.d@gmail.com>
2022-04-04 19:46:40 -0700

    add graph

---
+++ andrei/graph/edgelist.js
@@ -0,0 +1,14 @@
+// edge list
+
+const graph = [[0, 2], [2, 3], [2, 1], [1, 3]]
+
+// adjacent list
+const graph2 = [[2], [2, 3], [0, 1, 3], [1, 2]]
+
+// adjacent matrix
+const graph3 = {
+    0: [0, 0, 1, 0],
+    1: [0, 0, 1, 1],
+    2: [1, 1, 0, 1],
+    3: [0, 1, 1, 0]
+}
